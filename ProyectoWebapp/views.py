9bf1ca8ec1ce84ffd619bb116a7d0922dc974901
ahuamanpowercode <ahuaman@powercode.pe>
2024-03-20 00:25:07 -0500

    creacion del proyecto

---
+++ ProyectoWebapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse,redirect
+# Create your views here.
+def home(request):
+    return render(request, "proyectowebApp/home.html")
+
+def servicios(request):
+    return render(request, "proyectowebApp/servicios.html")
+
+def tienda(request):
+    return render(request, "proyectowebApp/tienda.html")
+
+def blog(request):
+    return render(request, "proyectowebApp/blog.html")
+
+def contacto(request):
+    return render(request, "proyectowebApp/contacto.html")
