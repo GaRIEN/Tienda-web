d7c5bc2f79fc354822ee9a58196641fccae60ddb
“GaRIEN” <gabrielhuaman683@gmail.com>
2024-03-22 10:30:40 -0500

    renderice servicios en la pagina servicios, tambien cambie en base, habia un error en el contenido que heredaan los otros templates

--- ProyectoWebapp/views.py
+++ ProyectoWebapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponse,redirect
+from servicios.models import  Servicios
 # Create your views here.
 def home(request):
     return render(request, "proyectowebApp/home.html")
 
 def servicios(request):
-    return render(request, "proyectowebApp/servicios.html")
+    servicios=Servicios.objects.all()
+    print(servicios)
+    return render(request, "proyectowebApp/servicios.html",{'servicios':servicios})
 
 def tienda(request):
     return render(request, "proyectowebApp/tienda.html")
